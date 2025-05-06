2004bc36da9bf2c8bc5a54b306e1a18e2d4833e3
NthienMinh <88758271+NthienMinh@users.noreply.github.com>
2025-05-06 17:24:36 +0700

    fix something

--- lib/features/products/views/products_view.dart
+++ lib/features/products/views/products_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:infinity_scroll/configs/color_configs.dart';
 import 'package:infinity_scroll/configs/text_style_configs.dart';
 import 'package:infinity_scroll/features/products/bloc/favorites/favorites_cubit.dart';
 import 'package:infinity_scroll/features/products/bloc/products_bloc.dart';
@@ -25,72 +26,104 @@ class ProductsView extends StatelessWidget {
       }
     });
     if (bloc.state is LoadFailure) {
-      return Center(child: Text(Texts.productsText.getDataFail));
+      return Center(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(Texts.productsText.getDataFail),
+          TextButton(onPressed: (){
+            bloc.add(LoadFirst());
+          }, child: Text(Texts.productsText.reload))
+        ],
+      ));
     }
     if (bloc.state is LoadError) {
-      return Center(child: Text(Texts.productsText.getDataError));
+      return Center(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(Texts.productsText.getDataError),
+          TextButton(onPressed: (){
+            bloc.add(LoadFirst());
+          }, child: Text(Texts.productsText.reload))
+        ],
+      ));
     }
 
-    return SingleChildScrollView(
-      controller: bloc.scrollController,
-      child: Column(
-        children: [
-          Padding(
+    return RefreshIndicator(onRefresh: ()async{
+      bloc.add(LoadFirst());
+    }, child: Stack(
+      children: [
+        SingleChildScrollView(
+          controller: bloc.scrollController,
+          child: Column(
+            children: [
+              SizedBox(
+                height: Resizable.size(context, 80)
+              ),
+              if (bloc.state is LoadingFirst)
+                const Center(child: CircularProgressIndicator()),
+              ...bloc.products.map(
+                    (e) => ListTile(
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(e.title, style: TextStyleConfigs.bigTextStyle(context)),
+                      Text(
+                        '${e.price}\$',
+                        style: TextStyleConfigs.smallTextStyle(context),
+                      ),
+                    ],
+                  ),
+                  trailing: BlocBuilder<FavoritesCubit, List<Product>>(
+                      bloc: favoritesCubit,
+                      builder: (c,s){
+                        return IconButton(
+                          onPressed: () {
+                            favoritesCubit.toggleFavorite(e);
+                          },
+                          icon: Icon(
+                              Icons.favorite,
+                              color:
+                              favoritesCubit.isFavorite(e) ? Colors.red : Colors.grey
+                          ),
+                        );
+                      }),
+                ),
+              ),
+              if (bloc.state is LoadingMore)
+                const Center(child: CircularProgressIndicator()),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: Resizable.size(context, 80),
+          child: Padding(
             padding: EdgeInsets.all(Resizable.padding(context, 10)),
             child: TextField(
+
               controller: bloc.searchController,
               onChanged: (value) {
                 bloc.add(SearchChanged());
               },
               decoration: InputDecoration(
+                filled: true,
+                fillColor: ColorConfigs.whiteColor,
+                border: const OutlineInputBorder(),
                 hintText: Texts.productsText.hintSearch,
                 suffixIcon:
-                    bloc.searchController.text.isNotEmpty
-                        ? IconButton(
-                          icon: const Icon(Icons.delete),
-                          onPressed: () {
-                            bloc.searchController.clear();
-                            bloc.add(LoadFirst());
-                          },
-                        )
-                        : null,
-              ),
-            ),
-          ),
-          if (bloc.state is LoadingFirst)
-            const Center(child: CircularProgressIndicator()),
-          ...bloc.products.map(
-            (e) => ListTile(
-              title: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(e.title, style: TextStyleConfigs.bigTextStyle(context)),
-                  Text(
-                    '${e.price}\$',
-                    style: TextStyleConfigs.smallTextStyle(context),
-                  ),
-                ],
-              ),
-              trailing: BlocBuilder<FavoritesCubit, List<Product>>(
-                  bloc: favoritesCubit,
-                  builder: (c,s){
-                return IconButton(
+                bloc.searchController.text.isNotEmpty
+                    ? IconButton(
+                  icon: const Icon(Icons.delete),
                   onPressed: () {
-                    favoritesCubit.toggleFavorite(e);
+                    bloc.searchController.clear();
+                    bloc.add(LoadFirst());
                   },
-                  icon: Icon(
-                      Icons.favorite,
-                      color:
-                      favoritesCubit.isFavorite(e) ? Colors.red : Colors.grey
-                  ),
-                );
-              }),
+                )
+                    : null,
+              ),
             ),
           ),
-          if (bloc.state is LoadingMore)
-            const Center(child: CircularProgressIndicator()),
-        ],
-      ),
-    );
+        )
+      ],
+    ));
   }
 }
--- lib/resource/texts/products_text.dart
+++ lib/resource/texts/products_text.dart
@@ -6,4 +6,5 @@ class ProductsText {
   final String getDataFail = "Lấy dữ liệu lỗi, vui lòng thử lại";
   final String getDataError = "Thiết bị có vấn đề, vui lòng kiểm tra và thử lại";
   final String hintSearch = "Tìm kiếm...";
+  final String reload = "Tải lại";
 }
